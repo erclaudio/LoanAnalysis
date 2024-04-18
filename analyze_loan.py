b2c900f169e973aa2ceeb5ccdce02060fa14f049
Claudiu Andrei <erfattone@gmail.com>
2024-04-19 00:05:45 +0100

    class definition

--- analyze_loan.py
+++ analyze_loan.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 import numpy_financial as npf 
 import pandas as pd 
 
+class Loan:
+
+    def __init__(self, rate, term, loan_amount, start = dt.date.today().isoformat()):
+         self.rate = rate/1200
+         self.periods = term *12
+         self.loan_amount = loan_amount
+         self.start = month_start(dt.date.fromisoformat(start)) + dt.datetime(31)
+         self.pmt = npf.pmt(self.rate, self.periods, -self.loan_amount)
